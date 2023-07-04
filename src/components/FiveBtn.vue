7aa2c6828d06e7e257f473f7f786e93dc3d00a19
euntaek419 <cet4713@gmail.com>
2023-07-05 02:38:10 +0900

    2023/07/05_ v-for 를 사용한 리팩토링

--- src/components/FiveBtn.vue
+++ src/components/FiveBtn.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- 
   <div>
     <button class="Btn2018" @click="Btn2018" :style="{ backgroundColor : Btnonoff[0], color: Btnonoff[6] }">2018</button>
     <button class="Btn2019" @click="Btn2019" :style="{ backgroundColor : Btnonoff[1], color: Btnonoff[6] }">2019</button>
@@ -7,14 +8,14 @@
     <button class="Btn2022" @click="Btn2022" :style="{ backgroundColor : Btnonoff[4], color: Btnonoff[6] }">2022</button>
     <button class="Btn2023" @click="Btn2023" :style="{ backgroundColor : Btnonoff[5], color: Btnonoff[6] }">2023</button>
   </div>
-
-<!--
-  <div>
-    <div v-for="item in list" :key="item">
-      <button :style="{ backgroundColor : Btnonoff[list]}"> {{ item }} </button>
-    </div>
-  </div>
 -->
+
+  <div>
+    <span v-for="(item, index) in list" :key="item">
+      <button @click="BtnClicked(index)" :style="{ backgroundColor : Btnonoff[index] }"> {{ item }} </button>
+    </span>
+  </div>
+
 
 </template>
 
@@ -24,95 +25,100 @@ export default {
         return {
             Btnonoff: ['','','','','','Blue','red'],
             counter:0,
-            // list:[2018,2019,2020,2021,2022,2023],
+            list:[2018,2019,2020,2021,2022,2023],
         }
     },
     methods:{
-        Btn2018() {
-            if(this.Btnonoff[0] == ''){
+        BtnClicked(index) {
+            if(this.Btnonoff[index] == ''){
                 this.counter = this.counter + 1
-                return this.Btnonoff[0] = 'Blue'
+                return this.Btnonoff[index] = 'Blue'
             }
             else{
-                if(this.counter == 0){
-                    return
+                if(this.counter !== 0){
+                    this.counter = this.counter - 1
+                    return this.Btnonoff[index] = ''
                 }
-                this.counter = this.counter - 1
-                return this.Btnonoff[0] = ''
             }
         },
 
-        Btn2019() {
-            if(this.Btnonoff[1] == ''){
-                this.counter = this.counter + 1
-                return this.Btnonoff[1] = 'Blue'
-            }
-            else{
-                if(this.counter == 0){
-                    return 
-                }
-                this.counter = this.counter - 1
-                return this.Btnonoff[1] = ''
-            }
-        },
-        Btn2020() {
-            if(this.Btnonoff[2] == ''){
-                this.counter = this.counter + 1
-                return this.Btnonoff[2] = 'Blue'
-            }
-            else{
-                if(this.counter == 0){
-                    this.counter = this.counter + 1
-                    return 
-                }
-                this.counter = this.counter - 1
-                return this.Btnonoff[2] = ''
-            }
-        },
-        Btn2021() {
-            if(this.Btnonoff[3] == ''){
-                this.counter = this.counter + 1
-                return this.Btnonoff[3] = 'Blue'
-            }
-            else{
-                if(this.counter == 0){
-                    return 
-                }
-                this.counter = this.counter - 1
-                return this.Btnonoff[3] = ''
-            }
-        },
-        Btn2022() {
-            if(this.Btnonoff[4] == ''){
-                this.counter = this.counter + 1
-                return this.Btnonoff[4] = 'Blue'
-            }
-            else{
-                if(this.counter == 0){
-                    return 
-                }
-                this.counter = this.counter - 1
-                return this.Btnonoff[4] = ''
-            }
-        },
-        Btn2023() {
-            if(this.Btnonoff[5] == ''){
-                this.counter = this.counter + 1
-                return this.Btnonoff[5] = 'Blue'
-            }
-            else{
-                if(this.counter == 0){
-                    return 
-                }
-                this.counter = this.counter - 1
-                return this.Btnonoff[5] = ''
-            }
-        },
+
+        // Btn2018() {
+        //     if(this.Btnonoff[0] == ''){
+        //         this.counter = this.counter + 1
+        //         return this.Btnonoff[0] = 'Blue'
+        //     }
+        //     else{
+        //         if(this.counter !== 0){
+        //             this.counter = this.counter - 1
+        //             return this.Btnonoff[0] = ''
+        //         }
+        //     }
+        // },
+        // Btn2019() {
+        //     if(this.Btnonoff[1] == ''){
+        //         this.counter = this.counter + 1
+        //         return this.Btnonoff[1] = 'Blue'
+        //     }
+        //     else{
+        //         if(this.counter !== 0){
+        //             this.counter = this.counter - 1
+        //             return this.Btnonoff[1] = '' 
+        //         }
+        //     }
+        // },
+        // Btn2020() {
+        //     if(this.Btnonoff[2] == ''){
+        //         this.counter = this.counter + 1
+        //         return this.Btnonoff[2] = 'Blue'
+        //     }
+        //     else{
+        //         if(this.counter !== 0){
+        //             this.counter = this.counter + 1
+        //             return this.Btnonoff[2] = ''
+        //         }
+        //     }
+        // },
+        // Btn2021() {
+        //     if(this.Btnonoff[3] == ''){
+        //         this.counter = this.counter + 1
+        //         return this.Btnonoff[3] = 'Blue'
+        //     }
+        //     else{
+        //         if(this.counter !== 0){
+        //             this.counter = this.counter - 1
+        //             return this.Btnonoff[3] = ''
+        //         }
+        //     }
+        // },
+        // Btn2022() {
+        //     if(this.Btnonoff[4] == ''){
+        //         this.counter = this.counter + 1
+        //         return this.Btnonoff[4] = 'Blue'
+        //     }
+        //     else{
+        //         if(this.counter !== 0){
+        //             this.counter = this.counter - 1
+        //             return this.Btnonoff[4] = ''
+        //         }
+        //     }
+        // },
+        // Btn2023() {
+        //     if(this.Btnonoff[5] == ''){
+        //         this.counter = this.counter + 1
+        //         return this.Btnonoff[5] = 'Blue'
+        //     }
+        //     else{
+        //         if(this.counter !== 0){
+        //             this.counter = this.counter - 1
+        //             return this.Btnonoff[5] = '' 
+        //         }
+        //     }
+        // },
     }
 
 }
 </script>
-
 
 <style>
 
